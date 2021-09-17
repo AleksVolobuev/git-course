0fbc13defc7ecdc43610641f28083808792dfbc3
AleksVolobuev <asketpsion@yandex.ru>
2021-09-17 08:41:56 +0300

    add second commit

---
+++ prog.go
@@ -0,0 +1,24 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	var quarter string
+// 	var year int
+// 	var width, height float64
+
+// 	quarter = "2021.4"
+// 	year = 2021
+// 	width, height = 2.4, 3.2
+
+// 	quarter := "2021.3"
+// 	year := 2021
+// 	width, height := 1.2, 2.3
+
+// 	fmt.Println(quarter)
+// 	fmt.Println(year)
+// 	fmt.Println(width, height)
+// 	fmt.Println(float64(year) + width)
+// }
